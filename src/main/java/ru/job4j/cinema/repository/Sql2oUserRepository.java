9534bdab0183ba2fefdfbbd14a05b42f2d01fb5e
Mouzzli <cokes03@yandex.ru>
2023-01-31 17:04:33 +0300

    added user registration

---
+++ src/main/java/ru/job4j/cinema/repository/Sql2oUserRepository.java
@@ -0,0 +1,44 @@
+package ru.job4j.cinema.repository;
+
+import org.springframework.stereotype.Repository;
+import org.sql2o.Sql2o;
+import ru.job4j.cinema.model.User;
+
+import java.util.Optional;
+
+@Repository
+public class Sql2oUserRepository implements UserRepository {
+    private static final String SAVE = "INSERT INTO users (full_name, email, password) VALUES (:fullName, :email, :password)";
+    private static final String FIND_BY_EMAIL_AND_PASSWORD = "SELECT * FROM users WHERE email = :email AND  password = :password";
+    private final Sql2o sql2o;
+
+    public Sql2oUserRepository(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public Optional<User> save(User user) {
+        try (var connection = sql2o.open()) {
+            var query = connection.createQuery(SAVE, true)
+                    .addParameter("fullName", user.getFullName())
+                    .addParameter("email", user.getEmail())
+                    .addParameter("password", user.getPassword());
+            int generatedKey = query.setColumnMappings(User.COLUMN_MAPPING).executeUpdate().getKey(Integer.class);
+            user.setId(generatedKey);
+            return Optional.of(user);
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<User> findByEmailAndPassword(String email, String password) {
+        try (var connection = sql2o.open()) {
+            var query = connection.createQuery(FIND_BY_EMAIL_AND_PASSWORD)
+                    .addParameter("email", email)
+                    .addParameter("password", password);
+            User user = query.executeAndFetchFirst(User.class);
+            return Optional.ofNullable(user);
+        }
+    }
+}
